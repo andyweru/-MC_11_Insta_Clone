340c2ba1e282198721d23479f6ac51720ca21981
Andrew Waweru <andyweru@gmail.com>
2018-10-08 16:39:46 +0300

    added bootstrap

--- gram/settings.py
+++ gram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'instagram.apps.InstagramConfig',
     'django.contrib.admin',
     'django.contrib.auth',
