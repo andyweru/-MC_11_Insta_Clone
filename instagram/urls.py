399df13bbe7fb5fc6227fd47cd050a4731989c5c
Andrew Waweru <andyweru@gmail.com>
2018-10-08 16:40:25 +0300

    added registration urls and login required

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns=[
     url('^$',views.entry,name = "entry"),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^logout/', views.logout, {"next_page": '/'}),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def entry(request):
     return render(request, "entry.html")
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    return render(request, "profile.html")
