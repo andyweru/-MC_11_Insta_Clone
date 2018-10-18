511898ecd455ac4e5dc753fb2eeeae79b6586036
Andrew Waweru <andyweru@gmail.com>
2018-10-18 11:03:44 +0300

    added route for individual pictures

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.entry,name = "entry"),
     url(r'^profile/', views.profile, name='profile'),
+    url(r'^post/(\d+)',views.post,name ='post'),
     url(r'^search/', views.search, name='search'),
     url(r'^upload/', views.upload, name='upload'),
     url(r'^logout/', views.logout, {"next_page": '/'}),
--- instagram/views.py
+++ instagram/views.py
@@ -50,4 +50,12 @@ def upload(request):
     else:
         form = NewImageForm()
     return render(request, 'upload.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def post(request,post_id):
+    try:
+        post = Image.objects.get(id = post_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"post.html", {"post":post})
         
